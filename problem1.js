26e9b3635250696586753144d008a4f21482a6e0
muhdsaifuddin2434-sketch <muhdsaifuddin2434@gmail.com>
2025-12-17 13:51:29 +0800

    Create problem1.js

---
+++ problem1.js
@@ -0,0 +1,19 @@
+ function selectionSort(arr) {
+let n = arr.length;
+for (let i = 0; i < n - 1; i++) {
+let minIndex = i;
+for (let j = i + 1; j < n; j++) {
+if (arr[j] < arr[minIndex]) {
+minIndex = j;
+}
+}
+if (minIndex !== i) {
+let temp = arr[i];
+arr[i] = arr[minIndex];
+arr[minIndex] = temp;
+}
+}
+return arr;
+}
+let numbers = [21, 400, 8, -3, 77, 99, -16, 55, 111, -36, 28];
+console.log(selectionSort(numbers));
